b688e60f3ab3d4d4c41a0e2dd94c24d38acd5877
vilaspatel <42242176+vilaspatel@users.noreply.github.com>
2020-10-29 14:23:07 +0530

    added tags and vnet name

--- main.tf
+++ main.tf
@@ -5,14 +5,21 @@ provider "azurerm" {
 resource "azurerm_resource_group" "aks" {
   name     = var.resourcegroupname
   location = var.location
+  tags = {
+    environment = "dev"
+  }
 }
 
 module "network" {
   source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.aks.name
   address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.1.0/24","10.0.2.0/24"]
-  subnet_names        = ["aks-gateway","aks-main"]
+  vnet_name           = '${var.prefix}-azurerm_resource_group.aks.name'
+  subnet_prefixes     = ["10.0.1.0/28","10.0.1.16/26","10.0.2.0/24"]
+  subnet_names        = ["aks-gateway","aks-dmz","aks-main"]
+  tags = {
+    environment = "dev"
+  }
   depends_on          = [azurerm_resource_group.aks]
 }
 
@@ -23,11 +30,13 @@ module "aks" {
   client_secret       = "your-service-principal-client-password"
   prefix              = var.prefix
   vnet_subnet_id      = module.network.vnet_subnets[0]
-  os_disk_size_gb     = 50
+  os_disk_size_gb     = 126
   agents_count        = var.agents_count
   agents_size         = var.agents_size
   enable_log_analytics_workspace = false
   # kubernetes_version  = var.kubernetes_version
-  
+  tags = {
+    environment = "dev"
+  }
   depends_on = [module.network]
 }
